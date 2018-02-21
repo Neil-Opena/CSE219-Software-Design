c4de0ebeefbce9cfcd6a90047292ee1fa68b5091
Neil Opena <neil1114o@gmail.com>
2018-02-21 11:21:28 -0500

    Added red round man

--- RedBoxMan_JavaFX/src/redboxman_javafx/RedBoxManRenderer.java
+++ RedBoxMan_JavaFX/src/redboxman_javafx/RedBoxManRenderer.java
@@ -21,6 +21,7 @@ public class RedBoxManRenderer extends Application {
     GraphicsContext gc;
     ArrayList<Point2D> imagesRedBoxManLocations;
     ArrayList<Point2D> shapesRedBoxManLocations;
+    ArrayList<Point2D> shapesRedRoundManLocations;
     Image redBoxManImage;
     
     @Override
@@ -28,6 +29,7 @@ public class RedBoxManRenderer extends Application {
 	// INIT THE DATA MANAGERS
 	imagesRedBoxManLocations = new ArrayList<>();
 	shapesRedBoxManLocations = new ArrayList<>();
+	shapesRedRoundManLocations = new ArrayList<>();
 	
 	// LOAD THE RED BOX MAN IMAGE
         InputStream str = getClass().getResourceAsStream("/RedBoxMan.png");
@@ -49,6 +51,9 @@ public class RedBoxManRenderer extends Application {
 	    }
 	    else if (e.isControlDown()) {
 		imagesRedBoxManLocations.add(new Point2D(e.getX(), e.getY()));
+		render();
+	    }else if(e.isAltDown()){
+		shapesRedRoundManLocations.add(new Point2D(e.getX(), e.getY()));
 		render();
 	    }
 	    else {
@@ -85,6 +90,9 @@ public class RedBoxManRenderer extends Application {
 	for (int j = 0; j < imagesRedBoxManLocations.size(); j++) {
 	    renderImageRedBoxMan(imagesRedBoxManLocations.get(j));
 	}
+	for (int k = 0; k < shapesRedRoundManLocations.size(); k++){
+		renderShapeRedRoundMan(shapesRedRoundManLocations.get(k));
+	}
     }
     
     public void renderShapeRedBoxMan(Point2D location) {
@@ -103,6 +111,79 @@ public class RedBoxManRenderer extends Application {
 	gc.stroke();
 	
 	// AND THEN DRAW THE REST OF HIM
+    }
+
+    public void renderShapeRedRoundMan(Point2D location){
+	    //Draw Body
+	   double bodyX = location.getX() + (25);
+	   double bodyY = location.getY() + (70);
+	   int bodyW = 60;
+	   int bodyH = 30;
+	   gc.setFill(Paint.valueOf("#000000"));
+	   gc.fillRect(bodyX, bodyY, bodyW, bodyH);
+	   gc.fillRect(bodyX + 7.5, bodyY + 20, 45, 20);
+	   gc.fillRect(bodyX - 2, bodyY + 35, 10, 10);
+	   gc.fillRect(bodyX + bodyW - 8, bodyY +35, 10, 10);
+
+
+
+	    String headColor = "#DD0000";
+	    String outlineColor = "#000000";
+	    int headW = 115;
+	    int headH = 88;
+
+	    //Draw his red head
+	    gc.setFill(Paint.valueOf(headColor));
+	    gc.fillOval(location.getX(), location.getY(), headW, headH);
+	    gc.beginPath();
+	    gc.setStroke(Paint.valueOf(outlineColor));
+	    gc.setLineWidth(1);
+	    gc.strokeOval(location.getX(), location.getY(), headW, headH);
+	    gc.stroke();
+
+	    //Draw his eyes
+	    String eyeColor = "#FFFF00";
+	    int eyeW = 30;
+	    int eyeH = 20;
+
+	    double leftX = location.getX() + (15);
+	    double leftY = location.getY() + (25);
+	    double rightX = location.getX() + (65);
+	    double rightY= leftY;
+
+	    gc.setFill(Paint.valueOf(eyeColor));
+	    gc.fillOval(leftX, leftY, eyeW, eyeH);
+	    gc.fillOval(rightX, rightY, eyeW, eyeH);
+	    gc.beginPath();
+	    gc.strokeOval(leftX, leftY, eyeW, eyeH);
+	    gc.strokeOval(rightX, rightY, eyeW, eyeH);
+	    gc.stroke();
+
+	    //Draw pupils
+	    String black = "#000000";
+	    int pupilW = 10;
+	    int pupilH = 5;
+	    double pupilLeftX = leftX + 9;
+	    double pupilLeftY = leftY + 8;
+	    double pupilRightX = rightX + 9;
+	    double pupilRightY = pupilLeftY;
+
+	    gc.setFill(Paint.valueOf(black));
+	    gc.fillOval(pupilLeftX, pupilLeftY, pupilW, pupilH);
+	    gc.fillOval(pupilRightX, pupilRightY, pupilW, pupilH);
+	    gc.beginPath();
+
+
+	   //Draw mouth
+	   double mouthX = location.getX() + 20;
+	   double mouthY = location.getY() + 60;
+	   int mouthW = 70;
+	   int mouthH = 7;
+	   gc.fillRect(mouthX, mouthY, mouthW, mouthH);
+	   gc.beginPath();
+
+	   
+
     }
     
     public void renderImageRedBoxMan(Point2D location) {
