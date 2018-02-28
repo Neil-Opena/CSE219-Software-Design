55f6873505c674ef7f0a1bf02139f6fdf375c1e5
Neil Opena <neil1114o@gmail.com>
2018-02-27 22:33:42 -0500

    Finished recitation 5

--- Recitation5/src/recitation5/Recitation5.java
+++ Recitation5/src/recitation5/Recitation5.java
@@ -4,11 +4,15 @@
  * and open the template in the editor.
  */
 package recitation5;
+import java.util.ArrayList;
 import javafx.application.Application;
+import javafx.collections.ObservableList;
 import javafx.scene.Scene;
 import javafx.scene.chart.LineChart;
 import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart;
+import javafx.scene.chart.XYChart.Data;
+import javafx.scene.chart.XYChart.Series;
 import javafx.stage.Stage;
 
 public class Recitation5 extends Application {
@@ -22,7 +26,8 @@ public class Recitation5 extends Application {
         xAxis.setLabel("Number of Month");
         yAxis.setLabel("Stock Price");
 
-        final LineChart lineChart = new LineChart(xAxis, yAxis);
+        //final LineChart<Number,Number> lineChart = new LineChart(xAxis, yAxis);
+	final LineChart lineChart = new LineChart(xAxis, yAxis);
 
         lineChart.setTitle("Stock Monitoring, 2010");
         
@@ -42,8 +47,47 @@ public class Recitation5 extends Application {
         series.getData().add(new XYChart.Data(11, 29));
         series.getData().add(new XYChart.Data(12, 25));
 
+	XYChart.Series series2 = new XYChart.Series();
+	series2.setName("Series 2");
+	series2.getData().add(new XYChart.Data(1, 3));
+        series2.getData().add(new XYChart.Data(2, 4));
+        series2.getData().add(new XYChart.Data(3, 5));
+        series2.getData().add(new XYChart.Data(4, 4));
+        series2.getData().add(new XYChart.Data(5, 4));
+        series2.getData().add(new XYChart.Data(6, 6));
+        series2.getData().add(new XYChart.Data(7, 2));
+        series2.getData().add(new XYChart.Data(8, 5));
+        series2.getData().add(new XYChart.Data(9, 3));
+        series2.getData().add(new XYChart.Data(10, 7));
+        series2.getData().add(new XYChart.Data(11, 9));
+        series2.getData().add(new XYChart.Data(12, 5));	
+
+	XYChart.Series series3 = new XYChart.Series();
+	series3.setName("YValueAdded");
+
         Scene scene = new Scene(lineChart, 800, 600);
         lineChart.getData().add(series);
+	lineChart.getData().add(series2);
+
+	ArrayList<Double> temp = new ArrayList<>(12);
+	for(int i = 0; i < 12; i++){
+		temp.add(0.0);
+	}
+
+	for(Series serie : (ObservableList<Series>) lineChart.getData()){
+		for(int i = 0; i < series.getData().size(); i++){
+			double oldVal = temp.get(i);
+			Data point = (Data) series.getData().get(i);
+			double y = Double.parseDouble(point.getYValue().toString());
+			temp.set(i, oldVal + y);
+		}
+	}
+
+	for(int i = 0; i < 12; i++){
+		series3.getData().add(new Data(i + 1, temp.get(i)));
+	}
+	lineChart.getData().add(series3);
+	lineChart.setCreateSymbols(false);
 
         primaryStage.setScene(scene);
         primaryStage.show();
