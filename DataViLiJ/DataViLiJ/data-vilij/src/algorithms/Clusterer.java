46f6bc473263f44279209d9842eeffe80f7e5f12
Neil Opena <neil1114o@gmail.com>
2018-04-30 16:29:01 -0400

    Added external code for clustering algorithms

--- DataViLiJ/DataViLiJ/data-vilij/src/algorithms/Clusterer.java
+++ DataViLiJ/DataViLiJ/data-vilij/src/algorithms/Clusterer.java
@@ -1,19 +1,30 @@
 package algorithms;
 
-import java.util.List;
-
 /**
- * An abstract class for classification algorithms. The output
- * for these algorithms is a straight line, as described in
- * Appendix C of the software requirements specification
- * (SRS). The {@link #output} is defined with extensibility
- * in mind.
+ * An abstract class for classification algorithms. The output for these
+ * algorithms is a straight line, as described in Appendix C of the software
+ * requirements specification (SRS). The {@link #output} is defined with
+ * extensibility in mind.
  *
+ * @author Ritwik Banerjee
+ */
+/**
  * @author Ritwik Banerjee
  */
 public abstract class Clusterer implements Algorithm {
 
-	protected List<String> labels;
+	protected final int numberOfClusters;
 
-	public abstract void modifyLabels();
+	public int getNumberOfClusters() {
+		return numberOfClusters;
+	}
+
+	public Clusterer(int k) {
+		if (k < 2) {
+			k = 2;
+		} else if (k > 4) {
+			k = 4;
+		}
+		numberOfClusters = k;
+	}
 }
